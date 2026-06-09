4906de4a6dc88d9a4a8e4ca449cb813d3713b8c2
steelfrontwa <steelfrontwar@gmail.com>
2026-06-09 14:15:45 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'tabel-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/styles.css',
+  './js/config.js',
+  './js/fonts.js',
+  './js/parser.js',
+  './js/calculator.js',
+  './js/pdf-generator.js',
+  './js/ui-renderer.js',
+  './js/app.js',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS).catch(() => {}))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request)
+        .catch(() => cached))
+  );
+});
